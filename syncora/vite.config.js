6178a251380f0279b58b0b9b773db6d0e66028bb
8gabrielly-a11y <8gabrielly@gmail.com>
2026-09-08 18:19:56 -0300

    Update vite.config.js

--- syncora/vite.config.js
+++ syncora/vite.config.js
@@ -1,19 +1,12 @@
-import path from 'node:path'\nimport base44 from "@base44/vite-plugin"
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
-// https://vite.dev/config/
-export default defineConfig({\n  resolve: { alias: { '@': path.resolve(process.cwd(), 'src') } },
-  plugins: [
-    base44({
-      // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-      // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
-    react(),
-  ]
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), 'src'),
+    },
+  },
 });
